f6e05851064c9229bd0f5c840559d38abac2dd5c
KFCC <KFCC@KFCC-no-MacBook-Air.local>
2015-04-12 11:21:50 +0900

    Initial Commit

---
+++ chikichiki3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chikichiki3
+//
+//  Created by KFCC on 2015/04/12.
+//  Copyright (c) 2015年 KFCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chikichiki3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chikichiki3
+//
+//  Created by KFCC on 2015/04/12.
+//  Copyright (c) 2015年 KFCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
